52c451df383151e0350f0fb17c6340e06e9c2972
Tung Nguyen <nthanhtung9x@gmail.com>
2020-07-25 19:15:12 +0700

    Updates

---
+++ precache-manifest.7d6092443c55e7b55ad7992de63cb4cd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf330089a2365a0e2ae58cee121fec2",
+    "url": "/TCREATION/index.html"
+  },
+  {
+    "revision": "9fca835bb293f8c62a6e",
+    "url": "/TCREATION/static/css/2.ba8cb563.chunk.css"
+  },
+  {
+    "revision": "cc193a7dbbfd1259e5b3",
+    "url": "/TCREATION/static/css/main.fee22ce7.chunk.css"
+  },
+  {
+    "revision": "9fca835bb293f8c62a6e",
+    "url": "/TCREATION/static/js/2.82a6aeda.chunk.js"
+  },
+  {
+    "revision": "4c3647ec769f81f0a9eaae3ac99b42a9",
+    "url": "/TCREATION/static/js/2.82a6aeda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc193a7dbbfd1259e5b3",
+    "url": "/TCREATION/static/js/main.c840c6cd.chunk.js"
+  },
+  {
+    "revision": "133363558f28ba0b51a3",
+    "url": "/TCREATION/static/js/runtime-main.40904029.js"
+  },
+  {
+    "revision": "4da244e24215cd179db9abfe1d4b1a4d",
+    "url": "/TCREATION/static/media/background.4da244e2.jpg"
+  },
+  {
+    "revision": "0170268fde21f89a03c9538b1e28ea89",
+    "url": "/TCREATION/static/media/banner.0170268f.png"
+  },
+  {
+    "revision": "54623c1f919a6ed039a2db02a20a10fa",
+    "url": "/TCREATION/static/media/contact.54623c1f.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/TCREATION/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/TCREATION/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/TCREATION/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/TCREATION/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b3ec82b711c862ee340c0ade69dc2348",
+    "url": "/TCREATION/static/media/slide1.b3ec82b7.png"
+  },
+  {
+    "revision": "853462f2fdf431061bbf05ecc30b3f66",
+    "url": "/TCREATION/static/media/slide2.853462f2.png"
+  },
+  {
+    "revision": "58b7b6fe7dfc6f14fd538297be0b3dc2",
+    "url": "/TCREATION/static/media/slide3.58b7b6fe.png"
+  }
+]);
